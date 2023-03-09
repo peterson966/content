cceab0e037e08316dd91da81ac9b5a7b0a8904ee
peterson966 <127274326+peterson966@users.noreply.github.com>
2023-03-09 02:56:48 -0300

    Update and rename files/en-us/learn/html/tables/index.md to arquivos/pt-br/aprender/html/tabelas/index.m

---
+++ arquivos/pt-br/aprender/html/tabelas/index.m
@@ -0,0 +1,69 @@
+	<!DOCTYPE HTML>
+<html>
+<cabeça>
+<title> Desenvolvimento Voltado a Web I </title>
+<meta charset =" utf-8 "/>
+<meta name =" viewport " content =" width=device-width, initial-scale=1, user-scalable=no " />
+<link rel =" folha de estilo " href =" assets/css/main.css " />
+</head>
+<body class =" is-preload ">
+<!-- Wrapper -->
+<div id =" wrapper ">
+<!-- Cabeçalho -->
+<cabeçalho id =" cabeçalho ">
+<div class =" logo ">
+</div>
+<div class =" conteúdo ">
+<div classe =" interior ">
+<h1> Curso: Tecnologia em Análise e Desenvolvimento de Sistemas <h1>
+<h2> Disciplina: Desenvolvimento Voltado a Web I </h2>
+</div>
+</div>
+<nav>
+<ul>
+<li>
+<div class =" menu suspenso ">
+<button class =" dropbtn "> Conteúdo </button>
+<div class =" conteúdo suspenso ">
+<a href =" # "> Aula 3 </a>
+<a href =" # "> Aula 4 </a>
+<a href =" # "> Aula 5 </a>
+<a href =" # "> Aula 6 </a>
+<a href =" # "> Aula 7 </a>
+<a href =" # "> Aula 8 </a>
+<a href =" # "> Sobre </a>
+</div>
+</div>
+</li>
+<li> <a href =" # "> Aulas 1 e 2 </a> </li>
+<li> <a href =" # "> Aula 3 </a> </li>
+<li> <a href =" # "> Aula 4 </a> </li>
+<li> <a href =" # "> Aula 5 </a> </li>
+<li> <a href =" # "> Aula 6 </a> </li>
+<li> <a href =" # "> Aula 7 </a> </li>
+<li> <a href =" # "> Aula 8 </a> </li>
+<li> <a href =" # "> Sobre </a> </li>
+</ul>
+</nav>
+</header>
+<!-- Rodapé -->
+< id do rodapé =" rodapé ">
+<h4> Redes Sociais </h4>
+<ul class =" ícones ">
+<li> <a href =" # " class =" ícone marcas fa-twitter "> <span class =" label "> Twitter </span> </a> </li>
+<li> <a href =" # " class =" ícone marcas fa-facebook-f "> <span class =" label "> Facebook </span> </a> </li>
+<li> <a href =" # " class =" ícone marcas fa-instagram "> <span class =" label "> Instagram </span> </a> </li>
+<li> <a href =" # " class =" ícone marcas fa-linkedin "> <span class =" label "> Linkedin </span> </a> </li>
+</ul>
+</rodapé>
+</div>
+<!-- BG -->
+<div id =" bg "> </div>
+<!-- Scripts -->
+<script src =" assets/js/jquery.min.js "> </script>
+<script src =" assets/js/browser.min.js "> </script>
+<script src =" assets/js/breakpoints.min.js "> </script>
+<script src =" assets/js/util.js "> </script>
+<script src =" assets/js/main.js "> </script>
+</body>
+</html>
